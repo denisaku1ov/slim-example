b3557d2ad810b56125d0a1c0003a9b57981ba0e1
denis <denisaku1ov@yandex.ru>
2020-12-05 12:33:25 +0300

    test

--- public/index.php
+++ public/index.php
@@ -11,6 +11,8 @@ use function Symfony\Component\String\s;
 
 $users = json_decode(file_get_contents('test.txt'));;
 //print_r($users);
+$availableIds = array_map(fn($user) => $user->id, $users);
+//print_r($availableIds);
 
 $container = new Container();
 $container->set('renderer', function () {
@@ -21,11 +23,13 @@ $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
+    $response->getBody()->write('<a href="/users">пользователи </a>');
     return $response;
     // Благодаря пакету slim/http этот же код можно записать короче
     // return $response->write('Welcome to Slim!');
 });
+
+
 
 $app->get('/users', function ($request, $response) use ($users) {
 
@@ -33,10 +37,10 @@ $app->get('/users', function ($request, $response) use ($users) {
         'users' => $users
     ];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-});
+})->setName('users');
+//^^ именнованный маршрут
 
-
-
+$router = $app->getRouteCollector()->getRouteParser();
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
@@ -45,20 +49,28 @@ $app->get('/courses/{id}', function ($request, $response, array $args) {
 
 
 $app->get('/users/new', function ($request, $response) use ($users) {
-    $id = count($users) + 1;
+    $id = count($users);
     $params = [
         'user' => ['name' => '', 'mail' => '', 'id' => $id],
     ];
     return $this->get('renderer')->render($response, 'users/new.phtml', $params);
 });
 
+$app->get('/users/{id}', function ($request, $response, array $args) use ($availableIds) {
+    $id = $args['id'];
+    if (in_array($id, $availableIds)) {
+        return $response->write("ID ПОЛЬЗОВАТЕЛЯ: {$id}");
+    }
+    return $response->withStatus(404)->write("<a href=\"/users/new\">Новый пользователь </a> <h1>ОШИБКА 404</h1>");
+});
 
-$app->post('/users', function ($request, $response) use ($users) {
+$app->post('/users', function ($request, $response) use ($users, $router) {
     $user = $request->getParsedBodyParam('user');
+    print_r($user);
     $users[] = $user;
     $encodedUsers = json_encode($users);
     file_put_contents('test.txt', $encodedUsers);
-    return $response->withRedirect('/users');
+    return $response->withRedirect($router->urlFor('users'));
 });
 
 
